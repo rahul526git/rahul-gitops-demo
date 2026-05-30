1408f7b017a738446e4cf8a40a1a437467962c52
Rahul_Jadon <rjadon946@gmail.com>
2026-05-30 08:29:11 +0000

    updated app port 9090

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "🐳 राहुल भाई की पहली लाइव GitOps ऐप एकदम फर्स्ट क्लास चल रही है! 🚀")
+}
+
 func main() {
-	fmt.Println("my first pipeline")
+	http.HandleFunc("/", helloHandler)
+	
+	// यहाँ हमने पोर्ट बदलकर 9090 कर दिया है
+	fmt.Println("Server starting on port 9090...")
+	http.ListenAndServe(":9090", nil)
 }
